29335585da3b2a82658245de2def768af57c9caa
beifengchuizoudemeng <1542072481@qq.com>
2018-11-08 17:04:58 +0800

    决策树告一段落

--- trees.py
+++ trees.py
@@ -99,3 +99,28 @@ def createTree (dataSet, labels):
         myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet,
         bestFeat, value), subLabels)
     return myTree
+
+#使用决策树的分类函数
+def classify(inputTree, featLabels, testVec):
+    firstStr = list(inputTree.keys())[0]
+    secondDict = inputTree[firstStr]
+    featIndex = featLabels.index(firstStr)#将标签字符串转换为索引
+    for key in secondDict.keys():
+        if testVec[featIndex] == key:
+            if type(secondDict[key]).__name__=='dict':
+                classLabel = classify(secondDict[key], featLabels, testVec)
+            else:
+                classLabel = secondDict[key]
+    return classLabel
+
+#使用pickle模块存储决策树
+def storeTree(inputTree, filename):
+    import pickle
+    fw = open(filename, 'wb')
+    pickle.dump(inputTree,fw)
+    fw.close()
+
+def grabTree(filename):
+    import pickle
+    fr = open(filename)
+    return pickle.load(fr)
